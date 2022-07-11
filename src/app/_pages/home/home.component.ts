e73d72f98d47a6f8846ef9c689ef6bd9684714b5
Pieter De Bie <Pieter.DeBie@bignited.be>
2022-07-11 14:12:39 +0200

    Working form with select-list

--- src/app/_pages/home/home.component.ts
+++ src/app/_pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  formCovidCases!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.formCovidCases = this.formBuilder.group({
+      'country':['',Validators.required],
+      'month' : ['',Validators.required]
+    })
+  }
+  onSubmit(){
+    if (this.formCovidCases.invalid)
+      return;
+    
+    console.log(this.formCovidCases.value)
   }
 
 }
